8420443ecc0968f81c3d71efacd2370327e6999f
Gregorius <kamandanug@gmail.com>
2024-01-18 13:59:35 +0700

    add index.html, style.css, script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+function getData(){
+    const options = {
+        method : 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cbc2b9d2a6msh09c81e8b8caccd7p108b37jsnb83cc689b8a4',
+		    'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+          }
+    }
+
+    let userInput = document.querySelector("input").value ; 
+
+    fetch(`https://covid-193.p.rapidapi.com/statistics?country=${userInput}`, options)
+    .then(response => response.json())
+    .then(data=> {
+       
+        console.log(data)
+        let errorValue = ``
+        if (data.errors.length <1 && data.response.length <1) {
+            errorValue = `Country: ${userInput} not found!`
+            document.getElementById('notif-error').innerHTML = errorValue
+
+            document.getElementById('activeCases').innerHTML = 'N/A'
+            document.getElementById('newCases').innerHTML ='N/A'
+            document.getElementById('recoveredCases').innerHTML = 'N/A'
+            document.getElementById('totalCases').innerHTML = 'N/A'
+            document.getElementById('totalDeaths').innerHTML = 'N/A'
+            document.getElementById('totalTests').innerHTML = 'N/A'
+        }else{
+            const activeCases = data.response[0]?.cases.active || 'N/A'
+            const newCases = data.response[0]?.cases.new || 'N/A'
+            const recoveredCases = data.response[0]?.cases.recovered || 'N/A'
+            const totalCases = data.response[0]?.tests.total || 'N/A'
+            const totalDeaths = data.response[0]?.deaths.total || 'N/A'
+            const totalTests = data.response[0]?.tests.total || 'N/A'
+    
+    
+            document.getElementById('activeCases').innerHTML = activeCases
+            document.getElementById('newCases').innerHTML = newCases
+            document.getElementById('recoveredCases').innerHTML = recoveredCases
+            document.getElementById('totalCases').innerHTML = totalCases
+            document.getElementById('totalDeaths').innerHTML = totalDeaths
+            document.getElementById('totalTests').innerHTML = totalTests
+
+            errorValue = ``
+            document.getElementById('notif-error').innerHTML = errorValue
+        }
+
+    })
+    .catch( err=>{
+        console.error(err)
+    })
+
+    document.querySelector("input").value = '';
+
+}
